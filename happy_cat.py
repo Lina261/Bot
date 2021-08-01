c2ea0076ed55430b019006bf29ed45835975ab96
Alina Kozyreva <alina0206200119@gmail.com>
2021-08-01 15:35:15 +0300

    add image to message

--- happy_cat.py
+++ happy_cat.py
@@ -1,9 +1,15 @@
-import telebot
+from PIL import Image
+from telebot import TeleBot
 from telebot import types
+import requests
+import shutil
 
+from io import BytesIO
 
-bot = telebot.TeleBot(TOKEN)
-mood = {'fun': 'Веселое', 'angry': 'Злое', 'sad': 'Грустное', 'anxious': 'Тревожное', 'depressed': 'Подавленное',
+cats_url = 'https://cataas.com/cat'
+
+bot = TeleBot()
+mood = {'angry': 'Злое', 'sad': 'Грустное', 'anxious': 'Тревожное', 'depressed': 'Подавленное',
         'joyful': 'Радостное', 'calm': 'Спокойное'}
 
 
@@ -24,6 +30,16 @@ def ask_about_mood(message):
     for k, v in mood.items():
         keyboard.add(types.InlineKeyboardButton(text=v, callback_data=k))
     bot.send_message(message.from_user.id, f'{name}, какое у тебя сегодня настроение?', reply_markup=keyboard)
+
+    @bot.callback_query_handler(func=lambda call: True)
+    def callback_worker(call):
+        print(call.data)
+        response = requests.get(cats_url)
+        img = Image.open(BytesIO(response.content))
+        bot.send_photo(photo=img, chat_id=call.message.chat.id, caption=generate_advice(call.data))
+
+    def generate_advice(user_mood):
+        print(user_mood)
 
 
 bot.polling()
